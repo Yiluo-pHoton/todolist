a415e38757006326eac40016e4b6a4f93416a6a0
Yiluo pHoton <yiluo.li.astrophysics@gmail.com>
2017-06-06 14:30:20 +0800

    Update views and urls

--- simple_todo/simple_todo/urls.py
+++ simple_todo/simple_todo/urls.py
@@ -12,9 +12,13 @@ Class-based views
 Including another URLconf
     1. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import include, url
-from django.contrib import admin
+from django.conf.urls import *
 
-urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-]
+urlpatterns = patterns(('simpleToDo.views'),
+	url(r'^$', 'todolist', name='todo'),
+	url(r'^addtodo/$', 'addTodo', name='add'),
+	url(r'^todofinish/(?P<id>\d+)/$', 'todofinish', name='finish'),
+	url(r'^todobackout/(?P<id>\d+)/$', 'todoback',  name='backout'),
+	url(r'^updatetodo/(?P<id>\d+)/$', 'updatetodo', name='update'),
+	url(r'^tododelete/(?P<id>\d+)/$', 'tododelete', name='delete'),
+)
--- simple_todo/todo/admin.py
+++ simple_todo/todo/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from simpleToDo.models import Todo
 
-# Register your models here.
+class TodoAdmin(admin.ModelAdmin):
+	list_display = ('user', 'todo', 'priority', 'flag', 'pubtime')
+	list_filter = ('pubtime',)
+	ordering = ('-pubtime',)
+
+
+admin.site.register(Todo, TodoAdmin)
